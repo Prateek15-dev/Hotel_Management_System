1e88f1aa75ced228e03da2ce40a9faf54e581899
Naman7600 <naman.vshah03@gmail.com>
2025-02-20 16:45:52 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,15 @@
+function viewUsers() {
+    window.location.href = 'view-users.html';
+}
+
+function viewRooms() {
+    window.location.href = 'view-rooms.html';
+}
+
+function viewOrders() {
+    window.location.href = 'view-orders.html';
+}
+
+function generateReports() {
+    window.location.href = 'reports.html';
+}
